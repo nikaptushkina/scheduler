30384493208e7346bf9d847e1fc1dd2ce26a9022
Nika Ptushkina <99292412+nika-ma@users.noreply.github.com>
2022-06-12 22:37:59 -0700

    Display appointments

--- src/components/Appointment/index.js
+++ src/components/Appointment/index.js
@@ -5,5 +5,10 @@ import Show from "./Show";
 import Empty from "./Empty";
 
 export default function Appointment(props) {
-  return <article className="appointment"></article>
+  return (
+    <article className="appointment">
+    <Header time={props.time} />
+    {props.interview ? <Show student={props.interview.student} interviewer={props.interview.interviewer}/> : <Empty />}
+    </article>
+  )
 }
