4ef824b975847fc6bba69a67014856f67eb0af49
Nika Ptushkina <99292412+nika-ma@users.noreply.github.com>
2022-06-21 18:10:22 -0700

    Mocking modules

--- src/hooks/useApplicationData.js
+++ src/hooks/useApplicationData.js
@@ -1,5 +1,5 @@
 import React, { useState , useEffect} from "react";
-const axios = require('axios');
+import axios from "axios";
 
 export default function useApplicationData() {
   const [state, setState] = useState({
